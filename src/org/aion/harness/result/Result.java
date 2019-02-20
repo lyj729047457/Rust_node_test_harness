da15d66031728b68c1cef95af3d74343cf507feb
aionick <nick@nuco.io>
2019-02-20 13:33:43 -0500

    Created new Result type
    - This type is a basic success/fail type but it also can hold an
      exception. This allows us to be able to pass the result to the
      caller and avoid having to throw all our exceptions, and the
      caller still has access to the stack trace for debugging.

---
+++ src/org/aion/harness/result/Result.java
@@ -0,0 +1,53 @@
+package org.aion.harness.result;
+
+/**
+ * A basic result that is either successful or unsuccessful.
+ *
+ * If unsuccessful, then this result will hold either a String with an error message or else it
+ * will hold an exception.
+ *
+ * If successful, then both the error string and the exception are meaningless and possibly null.
+ */
+public final class Result {
+    public final boolean success;
+    public final String error;
+    public final Exception exception;
+
+    private Result(boolean success, String error, Exception exception) {
+        this.success = success;
+        this.error = error;
+        this.exception = exception;
+    }
+
+    public static Result successful() {
+        return new Result(true, null, null);
+    }
+
+    public static Result unsuccessfulDueTo(String error) {
+        if (error == null) {
+            throw new NullPointerException("Cannot create result with null error.");
+        }
+
+        return new Result(false, error, null);
+    }
+
+    public static Result unsuccessfulDueToException(Exception e) {
+        if (e == null) {
+            throw new NullPointerException("Cannot create result with null exception.");
+        }
+
+        return new Result(false, e.toString(), e);
+    }
+    
+    @Override
+    public String toString() {
+        if (this.success) {
+            return "Result { successful }";
+        } else if (this.exception != null) {
+            return "Result { unsuccessful due to exception: " + this.exception.toString() + " }";
+        } else {
+            return "Result { unsuccessful due to: " + this.error + " }";
+        }
+    }
+
+}
