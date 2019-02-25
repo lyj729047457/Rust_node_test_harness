5dc42e710dae75bf774b75451ceed399db7bff15
aionick <nick@nuco.io>
2019-02-25 16:50:04 -0500

    Created TransactionReceipt and a builder class for it
    - The TransactionReceipt will be an object that users can fetch from an
      RPC call that will soon be supported.

---
+++ src/org/aion/harness/main/types/TransactionReceipt.java
@@ -0,0 +1,198 @@
+package org.aion.harness.main.types;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Optional;
+import org.aion.harness.kernel.Address;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * A transaction receipt holds pertinent information related to a transaction that has been sealed
+ * into a block in the blockchain.
+ *
+ * The preferred way of constructing instances of this class is {@link TransactionReceiptBuilder}.
+ *
+ * A transaction receipt is immutable.
+ */
+public final class TransactionReceipt {
+    private final long energyPrice;
+    private final long energyLimit;
+    private final long energyConsumed;
+    private final long cumulativeEnergyConsumed;
+    private final int transactionIndex;
+    private final byte[] blockHash;
+    private final byte[] bloomFilter;
+    private final byte[] transactionHash;
+    private final byte[] stateRootHash;
+    private final BigInteger blockNumber;
+    private final Address deployedContractAddress;
+    private final Address sender;
+    private final Address destination;
+
+    /**
+     * Constructs a new transaction receipt from the specified parameters.
+     */
+    public TransactionReceipt(long energyPrice, long energyLimit, long energyConsumed, long cumulativeEnergyConsumed,
+        int transactionIndex, byte[] blockHash, byte[] bloomFilter, byte[] transactionHash, byte[] stateRootHash,
+        BigInteger blockNumber, Address newContractAddress, Address sender, Address destination) {
+
+        this.energyPrice = energyPrice;
+        this.energyLimit = energyLimit;
+        this.energyConsumed = energyConsumed;
+        this.cumulativeEnergyConsumed = cumulativeEnergyConsumed;
+        this.transactionIndex = transactionIndex;
+        this.blockHash = Arrays.copyOf(blockHash, blockHash.length);
+        this.bloomFilter = Arrays.copyOf(bloomFilter, bloomFilter.length);
+        this.transactionHash = Arrays.copyOf(transactionHash, transactionHash.length);
+        this.stateRootHash = Arrays.copyOf(stateRootHash, stateRootHash.length);
+        this.blockNumber = blockNumber;
+        this.deployedContractAddress = newContractAddress;
+        this.sender = sender;
+        this.destination = destination;
+    }
+
+    /**
+     * Returns the energy price of the transaction. This is the amount per unit of energy that the
+     * sender paid per energy consumed.
+     *
+     * @return the transaction energy price.
+     */
+    public long getTransactionEnergyPrice() {
+        return this.energyPrice;
+    }
+
+    /**
+     * Returns the energy limit of the transaction. This is the maximum amount of energy the sender
+     * was willing to use.
+     *
+     * @return the transaction energy limit.
+     */
+    public long getTransactionEnergyLimit() {
+        return this.energyLimit;
+    }
+
+    /**
+     * Returns the amount of energy used by the transaction.
+     *
+     * @return the transaction energy price.
+     */
+    public long getTransactionEnergyConsumed() {
+        return this.energyConsumed;
+    }
+
+    /**
+     * Returns the cumulative amount of energy used by all transactions in the block that this
+     * transaction belongs to.
+     *
+     * @return the total amount of energy used.
+     */
+    public long getCumulativeEnergyConsumed() {
+        return this.cumulativeEnergyConsumed;
+    }
+
+    /**
+     * Returns the index in the block in which this transaction sits. A transaction at index i was
+     * the i'th transaction to be executed in the block.
+     *
+     * @return the transaction block index.
+     */
+    public int getTransactionIndex() {
+        return this.transactionIndex;
+    }
+
+    /**
+     * Returns the block hash of the block this transaction is sealed in.
+     *
+     * @return the block hash.
+     */
+    public byte[] getBlockHash() {
+        return Arrays.copyOf(this.blockHash, this.blockHash.length);
+    }
+
+    /**
+     * Returns the bloom filter corresponding to this transaction.
+     *
+     * @return the log bloom filter.
+     */
+    public byte[] getBloomFilter() {
+        return Arrays.copyOf(this.bloomFilter, this.bloomFilter.length);
+    }
+
+    /**
+     * Returns the transaction hash of the transaction that this receipt is describing.
+     *
+     * @return the transaction hash.
+     */
+    public byte[] getTransactionHash() {
+        return Arrays.copyOf(this.transactionHash, this.transactionHash.length);
+    }
+
+    /**
+     * Returns the state root hash at the time directly after this transaction was executed.
+     *
+     * @return the state root hash.
+     */
+    public byte[] getStateRootHash() {
+        return Arrays.copyOf(this.stateRootHash, this.stateRootHash.length);
+    }
+
+    /**
+     * Returns the block number of the block that this transaction was sealed in.
+     *
+     * @return the block number.
+     */
+    public BigInteger getBlockNumber() {
+        return this.blockNumber;
+    }
+
+    /**
+     * Returns an empty optional if the transaction was not a contract creation transaction.
+     *
+     * Otherwise, if the transaction created a new contract, this method returns the address of that
+     * new contract.
+     *
+     * @return the new contract address, if a new contract was created.
+     */
+    public Optional<Address> getAddressOfDeployedContract() {
+        return (this.deployedContractAddress == null) ? Optional.empty() : Optional.of(this.deployedContractAddress);
+    }
+
+    /**
+     * Returns the address of the account that sent this transaction.
+     *
+     * @return the sender address.
+     */
+    public Address getTransactionSender() {
+        return this.sender;
+    }
+
+    /**
+     * Returns an empty optional if no destination was specified (indicating the transaction is a
+     * contract creation transaction (see {@code getAddressOfDeployedContract()}).
+     *
+     * Otherwise, if the transaction had a specific destination address it was intended for, this
+     * method returns it.
+     *
+     * @return the destination address, if one was specified.
+     */
+    public Optional<Address> getTransactionDestination() {
+        return (this.destination == null) ? Optional.empty() : Optional.of(this.destination);
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionReceipt { energy limit = " + this.energyLimit
+            + ", energy price = " + this.energyPrice
+            + ", energy consumed = " + this.energyConsumed
+            + ", total energy consumed = " + this.cumulativeEnergyConsumed
+            + ", transaction index = " + this.transactionIndex
+            + ", block hash = 0x" + Hex.encodeHexString(this.blockHash)
+            + ", bloom filter = 0x" + Hex.encodeHexString(this.bloomFilter)
+            + ", transaction hash = 0x" + Hex.encodeHexString(this.transactionHash)
+            + ", state root hash = 0x" + Hex.encodeHexString(this.stateRootHash)
+            + ", block number = " + this.blockNumber
+            + ", deployed contract address = " + this.deployedContractAddress
+            + ", transaction sender = " + this.sender
+            + ", transaction destination = " + this.destination + " }";
+    }
+}
---
+++ src/org/aion/harness/main/types/TransactionReceiptBuilder.java
@@ -0,0 +1,345 @@
+package org.aion.harness.main.types;
+
+import java.math.BigInteger;
+import org.aion.harness.kernel.Address;
+import org.aion.harness.main.tools.RpcOutputParser;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * A builder class used to construct instances of {@link TransactionReceipt}.
+ *
+ * All initial values of the fields that will be used to construct the receipt are set to invalid
+ * values so that the receipt will throw an exception if a key field was missing.
+ *
+ * If subsequent calls to a method are made before a call to {@code build()} then the latest value
+ * will take precedence.
+ *
+ * All methods must be called before {@code build()} except for:
+ *   {@code newlyDeployedContractAddress()}
+ *   {@code transactionDestination()}
+ *
+ * The builder can be reused.
+ *
+ * To reset the builder back to its initial state use {@code clear()}.
+ *
+ * This class also provides a means of constructing a receipt from a Json String.
+ *
+ * This class is not thread-safe.
+ */
+public final class TransactionReceiptBuilder {
+    private long energyPrice = -1;
+    private long energyLimit = -1;
+    private long energyConsumed = -1;
+    private long cumulativeEnergyConsumed = -1;
+    private int transactionIndex = -1;
+    private byte[] blockHash = null;
+    private byte[] bloomFilter = null;
+    private byte[] transactionHash = null;
+    private byte[] stateRootHash = null;
+    private BigInteger blockNumber = null;
+    private Address deployedContractAddress = null;
+    private Address sender = null;
+    private Address destination = null;
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the energy price of the transaction.
+     *
+     * @param energyPrice The energy price.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder transactionEnergyPrice(long energyPrice) {
+        this.energyPrice = energyPrice;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the energy limit of the transaction.
+     *
+     * @param energyLimit The energy limit.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder transactionEnergyLimit(long energyLimit) {
+        this.energyLimit = energyLimit;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the amount of energy consumed of the transaction.
+     *
+     * @param energyConsumed The energy consumed.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder energyConsumedByTransaction(long energyConsumed) {
+        this.energyConsumed = energyConsumed;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the amount of energy consumed by the block this transaction belongs to.
+     *
+     * @param cumulativeEnergyConsumed The total amount of energy consumed.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder totalEnergyConsumedByBlock(long cumulativeEnergyConsumed) {
+        this.cumulativeEnergyConsumed = cumulativeEnergyConsumed;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the transaction index.
+     *
+     * @param transactionIndex The transaction index.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder indexOfTransactionInBlock(int transactionIndex) {
+        this.transactionIndex = transactionIndex;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the block hash.
+     *
+     * @param blockHash The block hash.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder blockHash(byte[] blockHash) {
+        this.blockHash = blockHash;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the bloom filter of the transaction.
+     *
+     * @param bloomFilter The bloom filter.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder bloomFilter(byte[] bloomFilter) {
+        this.bloomFilter = bloomFilter;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the transaction hash.
+     *
+     * @param transactionHash The transaction hash.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder transactionHash(byte[] transactionHash) {
+        this.transactionHash = transactionHash;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the hash of the storage state root.
+     *
+     * @param stateRootHash The state root hash.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder stateRootHash(byte[] stateRootHash) {
+        this.stateRootHash = stateRootHash;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the block number of the block the transaction belongs to.
+     *
+     * @param blockNumber The block number.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder blockNumber(BigInteger blockNumber) {
+        this.blockNumber = blockNumber;
+        return this;
+    }
+
+    /**
+     * <b>Mandatory. This field must be set.</b>
+     *
+     * Sets the sender of the transaction.
+     *
+     * @param sender The transaction sender.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder transactionSender(Address sender) {
+        this.sender = sender;
+        return this;
+    }
+
+    /**
+     * <b>Semi-optional. This field may or may not be set: but either it or destination must be set,
+     * and both cannot be set.</b>
+     *
+     * Sets the address of the contract that the transaction deployed.
+     *
+     * @param deployedContractAddress The address of the newly deployed contract.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder newlyDeployedContractAddress(Address deployedContractAddress) {
+        this.deployedContractAddress = deployedContractAddress;
+        return this;
+    }
+
+    /**
+     * <b>Semi-optional. This field may or may not be set: but either it or the newly deployed
+     * contract address must be set, and both cannot be set.</b>
+     *
+     * Sets the energy price of the transaction.
+     *
+     * @param destination The destination address of the transaction.
+     * @return this builder.
+     */
+    public TransactionReceiptBuilder transactionDestination(Address destination) {
+        this.destination = destination;
+        return this;
+    }
+
+    /**
+     * Builds the receipt from the parameters that were set prior to calling this method.
+     *
+     * @return the newly built receipt.
+     */
+    public TransactionReceipt build() {
+        if (this.energyPrice < 1) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no energy price set.");
+        }
+        if (this.energyLimit < 0) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no energy limit set.");
+        }
+        if (this.energyConsumed < 0) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no energy consumed set.");
+        }
+        if (this.cumulativeEnergyConsumed < 0) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no cumulative energy consumed set.");
+        }
+        if (this.transactionIndex < 0) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no transaction index set.");
+        }
+        if (this.blockHash == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no block hash set.");
+        }
+        if (this.bloomFilter == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no bloom filter set.");
+        }
+        if (this.transactionHash == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no transaction hash set.");
+        }
+        if (this.stateRootHash == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no state root hash set.");
+        }
+        if (this.blockNumber == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no block number set.");
+        }
+        if (this.sender == null) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no sender set.");
+        }
+        if ((this.deployedContractAddress == null) && (this.destination == null)) {
+            throw new IllegalStateException("Cannot construct transaction receipt - no destination AND no contract address set.");
+        }
+        if ((this.deployedContractAddress != null) && (this.destination != null)) {
+            throw new IllegalStateException("Cannot construct transaction receipt - both destination AND contract address set.");
+        }
+
+        return new TransactionReceipt(
+            this.energyPrice,
+            this.energyLimit,
+            this.energyConsumed,
+            this.cumulativeEnergyConsumed,
+            this.transactionIndex,
+            this.blockHash,
+            this.bloomFilter,
+            this.transactionHash,
+            this.stateRootHash,
+            this.blockNumber,
+            this.deployedContractAddress,
+            this.sender,
+            this.destination
+        );
+    }
+
+    /**
+     * Builds a {@link TransactionReceipt} from a provided Json String, where it is assumed that the
+     * provided string has all of the attributes necessary to construct this object.
+     *
+     * In general, this method should only ever be called with the output result of an RPC method
+     * that returns a transaction receipt in Json format.
+     *
+     * Internally, this method calls all of the build methods in this class and builds the receipt,
+     * but handles all of the parsing on its own.
+     *
+     * @param jsonString The json string to build the receipt from.
+     * @return the transaction receipt.
+     */
+    public TransactionReceipt buildFromJsonString(String jsonString) throws DecoderException {
+        RpcOutputParser jsonParser = new RpcOutputParser(jsonString);
+
+        String energyPrice = jsonParser.attributeToString("nrgPrice");
+        String energyLimit = jsonParser.attributeToString("gasLimit");
+        String energyUsed = jsonParser.attributeToString("nrgUsed");
+        String totalEnergyUsed = jsonParser.attributeToString("cumulativeGasUsed");
+        String index = jsonParser.attributeToString("transactionIndex");
+        String blockHash = jsonParser.attributeToString("blockHash");
+        String bloomFilter = jsonParser.attributeToString("logsBloom");
+        String transactionHash = jsonParser.attributeToString("transactionHash");
+        String rootHash = jsonParser.attributeToString("root");
+        String blockNumber = jsonParser.attributeToString("blockNumber");
+        String sender = jsonParser.attributeToString("from");
+        String contract = jsonParser.attributeToString("contractAddress");
+        String destination = jsonParser.attributeToString("to");
+
+        return new TransactionReceiptBuilder()
+            .transactionEnergyPrice((energyPrice == null) ? -1 : new BigInteger(energyPrice, 16).longValueExact())
+            .transactionEnergyLimit((energyLimit == null) ? -1 : new BigInteger(energyLimit, 16).longValueExact())
+            .energyConsumedByTransaction((energyUsed == null) ? -1 : new BigInteger(energyUsed, 16).longValueExact())
+            .totalEnergyConsumedByBlock((totalEnergyUsed == null) ? -1 : new BigInteger(totalEnergyUsed, 16).longValueExact())
+            .indexOfTransactionInBlock((index == null) ? -1 : new BigInteger(index, 16).intValueExact())
+            .blockHash((blockHash == null) ? null : Hex.decodeHex(blockHash))
+            .bloomFilter((bloomFilter == null) ? null : Hex.decodeHex(bloomFilter))
+            .transactionHash((transactionHash == null) ? null : Hex.decodeHex(transactionHash))
+            .stateRootHash((rootHash == null) ? null : Hex.decodeHex(rootHash))
+            .blockNumber((blockNumber == null) ? null : new BigInteger(blockNumber, 16))
+            .transactionSender((sender == null) ? null : new Address(Hex.decodeHex(sender)))
+            .newlyDeployedContractAddress((contract == null) ? null : new Address(Hex.decodeHex(contract)))
+            .transactionDestination((destination == null) ? null : new Address(Hex.decodeHex(destination)))
+            .build();
+    }
+
+    /**
+     * Restores this builder to its initial empty state.
+     */
+    public void clear() {
+        this.energyPrice = -1;
+        this.energyLimit = -1;
+        this.energyConsumed = -1;
+        this.cumulativeEnergyConsumed = -1;
+        this.transactionIndex = -1;
+        this.blockHash = null;
+        this.bloomFilter = null;
+        this.transactionHash = null;
+        this.stateRootHash = null;
+        this.blockNumber = null;
+        this.deployedContractAddress = null;
+        this.sender = null;
+        this.destination = null;
+    }
+
+}
