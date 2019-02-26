9c663a187019ff7ad45a50fd0e1823de259e68c8
aionick <nick@nuco.io>
2019-02-26 11:40:57 -0500

    NodeListener can now wait for syncing to complete

--- src/org/aion/harness/main/NodeListener.java
+++ src/org/aion/harness/main/NodeListener.java
@@ -1,10 +1,16 @@
 package org.aion.harness.main;
 
+import java.math.BigInteger;
+import java.text.NumberFormat;
 import org.aion.harness.main.event.Event;
 import org.aion.harness.main.event.IEvent;
 import org.aion.harness.main.event.OrEvent;
 import org.aion.harness.main.global.SingletonFactory;
+import org.aion.harness.main.types.SyncStatus;
+import org.aion.harness.misc.Assumptions;
 import org.aion.harness.result.LogEventResult;
+import org.aion.harness.result.Result;
+import org.aion.harness.result.RpcResult;
 import org.aion.harness.util.EventRequest;
 import org.aion.harness.util.IEventRequest;
 import org.aion.harness.util.LogListener;
@@ -120,6 +126,67 @@ public final class NodeListener {
     }
 
     /**
+     * Blocks until the node has finished syncing with the rest of the network, or until the request
+     * times out.
+     *
+     * Technically speaking, a node never finishes syncing with the network unless it is always
+     * at the top block and always the first to mine the next block. However, once at the top of
+     * the chain, a node is in a relatively stable position with regards to the network. Therefore
+     * this method considers a node in sync with the network if it is within 5 blocks of the top
+     * of the chain.
+     *
+     * @param delayInMillis The amount of time to wait between checking the current sync status.
+     * @param timeoutInMillis The total amount of time to wait for syncing.
+     * @return  the result of this event.
+     */
+    public Result waitForSyncToComplete(long delayInMillis, long timeoutInMillis) {
+        if (timeoutInMillis < 0) {
+            throw new IllegalArgumentException("Timeout value was negative: " + timeoutInMillis);
+        }
+
+        RPC rpc = new RPC();
+
+        try {
+            long currentTime = System.currentTimeMillis();
+            long deadline = currentTime + timeoutInMillis;
+
+            RpcResult<SyncStatus> syncStatus = rpc.getSyncingStatus();
+            if (!syncStatus.success) {
+                return Result.unsuccessfulDueTo(syncStatus.error);
+            }
+
+            while ((currentTime < deadline) && (syncStatus.getResult().isSyncing())) {
+                // Log the current status.
+                SyncStatus status = syncStatus.getResult();
+                broadcastSyncUpdate(status.isWaitingToConnect(), status.getSyncCurrentBlockNumber(), status.getHighestBlockNumber());
+
+                // Sleep for the specified delay, unless there is less time remaining until the
+                // deadline, then sleep only until the deadline.
+                Thread.sleep(Math.min(deadline - currentTime, delayInMillis));
+
+                // Update the status.
+                syncStatus = rpc.getSyncingStatus();
+                if (!syncStatus.success) {
+                    return Result.unsuccessfulDueTo(syncStatus.error);
+                }
+
+                currentTime = System.currentTimeMillis();
+            }
+
+            // We either timed out or finished syncing.
+            if (currentTime >= deadline) {
+                return Result.unsuccessfulDueTo("Timed out waiting for sync to finish.");
+            } else {
+                System.out.println(syncStatus);
+                return Result.successful();
+            }
+
+        } catch (InterruptedException e) {
+            return Result.unsuccessfulDueToException(e);
+        }
+    }
+
+    /**
      * Returns the number of events that are currently being listened for. These events may have
      * been requested by separate {@link NodeListener} objects. But these are the total number
      * currently being processed.
@@ -128,6 +195,16 @@ public final class NodeListener {
      */
     public static int numberOfEventsBeingListenedFor() {
         return SingletonFactory.singleton().logReader().getLogListener().numberOfPendingEventRequests();
+    }
+
+    private void broadcastSyncUpdate(boolean waitingToConnect, BigInteger currentBlock, BigInteger highestBlock) {
+        if (waitingToConnect) {
+            System.out.println(Assumptions.LOGGER_BANNER + "Sync Process = { waiting to connect to peers }");
+        } else {
+            System.out.println(Assumptions.LOGGER_BANNER + "Sync Progress = { At block: "
+                + NumberFormat.getIntegerInstance().format(currentBlock)
+                + " of " + NumberFormat.getIntegerInstance().format(highestBlock) + " }");
+        }
     }
 
     private LogEventResult extractResult(IEventRequest request) {
