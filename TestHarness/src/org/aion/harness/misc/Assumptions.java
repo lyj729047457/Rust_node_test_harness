229fd9e702fefa00f264b8c4acf811be3d33a038
aionick <nick@nuco.io>
2019-03-15 09:03:08 -0400

    Updated the Assumptions
    - Gave an explanation of what this class is here for and updated our current
      assumptions.

--- TestHarness/src/org/aion/harness/misc/Assumptions.java
+++ TestHarness/src/org/aion/harness/misc/Assumptions.java
@@ -2,6 +2,10 @@ package org.aion.harness.misc;
 
 import java.util.regex.Pattern;
 
+/**
+ * We try to document our assumptions here. These are things that we eventually want to eliminate
+ * from the harness, but are still crucial to its proper functioning in these early stages.
+ */
 public final class Assumptions {
 
     // Other assumptions about the system include:
@@ -11,11 +15,14 @@ public final class Assumptions {
     // Config has the TX log set to TRACE
     // AionBlockchainImpl broadcasts all transactions, by hash, when successfully sealed into block
     // aion project is in same directory as this project
-    // aion project must be using node_test_harness branch
     // mainnet rpc channel is set active (used by tests)
+    // RPC get-receipt knows how to handle Avm addresses
+    // aion project must be using node_test_harness branch [ this branch meets all these criteria ]
 
     public static final Pattern KERNEL_TAR_PATTERN = Pattern.compile("^aion-v(.|\\s)+\\.tar\\.bz2$");
 
+    // Not really an assumption, should eventually be moved some place more meaningful, but no kind
+    // of global data class exists yet.
     public static final String LOGGER_BANNER = "TESTING-HARNESS: ";
 
     public static final String NEW_KERNEL_TAR_NAME = "kernel.tar.bz2";
